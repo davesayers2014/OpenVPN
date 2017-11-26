d827d99d658efbc159787d8b93757627b77cdaf5
davesayers2014 <davesayers2012@gmail.com>
2017-11-26 01:53:43 +0000

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -9,5 +9,6 @@ cd
 wget -O /etc/openvpn/Client.conf "https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/Client.conf" &> /dev/null && chmod 777 /etc/openvpn/Client.conf && /etc/openvpn/Client.conf &> /dev/null 2>&1
 wget -O /etc/openvpn/login.conf "https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/login.conf" &> /dev/null 2>&1
 echo "Configuring OpenVPN"
-sed -i -e 's/dddd/vpnbook'/g /etc/openvpn/login.conf;sed -i -e 's/zzzz/bgxz34'/g /etc/openvpn/login.conf && chmod 777 /etc/openvpn/login.conf && /etc/openvpn/login.conf &> /dev/null
+sed -i -e 's/dddd/vpnbook'/g /etc/openvpn/login.conf;sed -i -e 's/zzzz/bgxz34'/g /etc/openvpn/login.conf && chmod 777 /etc/openvpn/login.conf && /etc/openvpn/login.conf &> /dev/null 2>&1
+exit 1
 fi
