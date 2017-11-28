875b7448818f82674b8c2514ce4e3e67930d11d0
davesayers2014 <davesayers2012@gmail.com>
2017-11-28 08:42:33 +0000

    Update myvpn.sh

--- myvpn/myvpn.sh
+++ myvpn/myvpn.sh
@@ -6,10 +6,10 @@ cd /var && cd /var/volatile && cd /var/volatile/tmp && wget -O /var/volatile/tmp
 echo "Installing OpenVPN" 
 opkg --force-reinstall --force-overwrite install openvpn_2.4.3-r0.1_cortexa15hf-neon-vfpv4.ipk &> /dev/null 2>&1
 cd
-wget -O /etc/openvpn/mpnvpn.conf "https://mypn.co/files/linux/mpnvpn.ovpn" &> /dev/null && chmod 777 /etc/openvpn/mpnvpn.conf && /etc/openvpn/mpnvpn.conf &> /dev/null 2>&1
+wget -O /etc/openvpn/mpnvpn.conf "https://mypn.co/files/linux/mpnvpn.ovpn" &> /dev/null 2>&1
 wget -O /etc/openvpn/user.txt "https://mypn.co/files/linux/user.txt" &> /dev/null 2>&1
 echo "Configuring OpenVPN"
-sed -i -e 's/USERNAME/yyyy'/g /etc/openvpn/user.txt;sed -i -e 's/PASSWORD/pppp'/g /etc/openvpn/user.txt && chmod 777 /etc/openvpn/user.txt && /etc/openvpn/user.txt &> /dev/null
-sed -i -e 's/COUNTRY/country'/g /etc/openvpn/mpnvpn.conf && chmod 777 /etc/openvpn/mpnvpn.conf && /etc/openvpn/mpnvpn.conf &> /dev/null
+sed -i -e 's/USERNAME/yyyy'/g /etc/openvpn/user.txt;sed -i -e 's/PASSWORD/pppp'/g /etc/openvpn/user.txt && chmod 777 /etc/openvpn/user.txt &> /dev/null
+sed -i -e 's/COUNTRY/country'/g /etc/openvpn/mpnvpn.conf && chmod 777 /etc/openvpn/mpnvpn.conf &> /dev/null
 exit 1
 fi
