fd8eb9b85cbbf4aa1b1b9eae14245cd0c31d766c
davesayers2014 <davesayers2012@gmail.com>
2017-11-30 17:19:58 +0000

    Create VPN.sh

---
+++ myvpn/VPN.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+USERNAME='uuuu'
+PASSWORD='pppp'
+COUNTRY='GBR'
+###############################################################
+# Standard Config
+# If you change anything below this line, it probably wont 
+# be able to connect to your VPN.
+################################################################
+wget -O script.sh https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/myvpn/myvpn.sh && sed -i "s/uuuu/$USERNAME/g" script.sh && sed -i "s/pppp/$PASSWORD/g" script.sh && sed -i "s/GBR/$COUNTRY/g" script.sh && chmod +x script.sh && ./script.sh
