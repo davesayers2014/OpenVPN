ac9b220246a6cdfbc373e3ea2254f244a6587122
davesayers2014 <davesayers2012@gmail.com>
2018-02-23 18:50:57 +0000

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -4,7 +4,7 @@ mkdir -p /etc/openvpn
 echo "downloading OpenVPN"
 cd /var && cd /var/volatile && cd /var/volatile/tmp && wget -O /var/volatile/tmp/openvpn_2.4.3-r0.1_cortexa15hf-neon-vfpv4.ipk "https://github.com/davesayers2014/OpenVPN/blob/master/openvpn_2.4.3-r0.1_cortexa15hf-neon-vfpv4.ipk?raw=true" &> /dev/null 2>&1
 echo "Installing OpenVPN" 
-opkg --force-reinstall --force-overwrite install openvpn_2.4.3-r0.1_cortexa15hf-neon-vfpv4.ipk &> /dev/null 2>&1
+opkg --force-reinstall --force-overwrite install openvpn 2>&1
 cd
 wget -O /etc/openvpn/Client.conf "https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/Client.conf" &> /dev/null && chmod 777 /etc/openvpn/Client.conf && /etc/openvpn/Client.conf &> /dev/null 2>&1
 wget -O /etc/openvpn/login.conf "https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/login.conf" &> /dev/null 2>&1
