b0be1664f69c2dc14f772a771a1565a17b296347
davesayers2014 <davesayers2012@gmail.com>
2018-02-23 19:45:16 +0000

    Update VPN.sh

--- VPN.sh
+++ VPN.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-USERNAME='uuuu'
-PASSWORD='pppp'
-COUNTRY='GBR'
+USERNAME='USERNAME'
+PASSWORD='PASSWORD'
 ###############################################################
 # Standard Config
 # If you change anything below this line, it probably wont 
 # be able to connect to your VPN.
 ################################################################
-wget -O script.sh https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/myvpn/myvpn.sh && sed -i "s/uuuu/$USERNAME/g" script.sh && sed -i "s/pppp/$PASSWORD/g" script.sh && sed -i "s/GBR/$COUNTRY/g" script.sh && chmod +x script.sh && ./script.sh
+wget -O VPNBook.sh https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/VPNBook.sh && sed -i "s/USERNAME/$USERNAME/g" script.sh && sed -i "s/PASSWORD/$PASSWORD/g" script.sh && chmod +x VPNBook.sh && ./VPNBook.sh
