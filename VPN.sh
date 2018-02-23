3cdc3313c1aa6e08d73120020b8678fea621f2d1
davesayers2014 <davesayers2012@gmail.com>
2018-02-23 20:42:02 +0000

    Update VPN.sh

--- VPN.sh
+++ VPN.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-USERNAME='USERNAME'
-PASSWORD='PASSWORD'
+USERNAME='vpnbook'
+PASSWORD='5grtx8k'
 ###############################################################
 # Standard Config
 # If you change anything below this line, it probably wont 
 # be able to connect to your VPN.
 ################################################################
-wget -O VPNBook.sh https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/VPNBook.sh && sed -i "s/USERNAME/$USERNAME/g" script.sh && sed -i "s/PASSWORD/$PASSWORD/g" script.sh && chmod +x VPNBook.sh && ./VPNBook.sh
+wget -O VPNBook.sh https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/VPNBook.sh && sed -i "s/uuuu/$USERNAME/g" VPNBook.sh && sed -i "s/pppp/$PASSWORD/g" VPNBook.sh && chmod +x VPNBook.sh && ./VPNBook.sh
