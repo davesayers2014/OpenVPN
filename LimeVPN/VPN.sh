637d6be189834aac63f48389b5b1b52502094502
davesayers2014 <davesayers2012@gmail.com>
2018-12-14 08:36:19 +0000

    Setup LimeVPN

---
+++ LimeVPN/VPN.sh
@@ -0,0 +1,8 @@
+USERNAME='uuuu'
+PASSWORD='pppp'
+###############################################################
+# Standard Config
+# If you change anything below this line, it probably wont 
+# be able to connect to your VPN.
+################################################################
+wget -O LimeVPN.sh https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/LimeVPN/Nord.sh && sed -i "s/uuuu/$USERNAME/g" LimeVPN.sh && sed -i "s/pppp/$PASSWORD/g" LimeVPN.sh && chmod +x LimeVPN.sh && ./LimeVPN.sh
