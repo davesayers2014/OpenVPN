3a83e467a168c4257b9c23d057fbecdd1916b8f2
davesayers2014 <davesayers2012@gmail.com>
2019-06-03 02:08:53 +0100

    Add MonsterVPN

---
+++ MonsterVPN/Monster.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+echo "check_certificate = off" >> ~/.wgetrc
+USERNAME='uuuu'
+PASSWORD='pppp'
+###############################################################
+# Standard Config
+# If you change anything below this line, it probably wont 
+# be able to connect to your VPN.
+################################################################
+###############################################################
+# Standard Config
+# If you change anything below this line, it probably wont 
+# be able to connect to your VPN.
+################################################################
+rm -rv /etc/openvpn >/dev/null 2>&1
+rm -v /hdd/MonsterVPN.zip >/dev/null 2>&1
+rm -rv /hdd/MonsterVPN >/dev/null 2>&1
+mkdir -p /hdd/MonsterVPN
+mkdir -p /etc/openvpn
+
+# Download and install VPN Changer
+echo "downloading VPN Changer"
+echo $LINE
+cd /var && cd /var/volatile && cd /var/volatile/tmp && wget -O /var/volatile/tmp/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk "https://github.com/davesayers2014/OpenVPN/blob/master/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk?raw=true" &> /dev/null 2>&1
+echo "Installing VPN Changer"
+echo $LINE
+opkg --force-reinstall --force-overwrite install enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk &> /dev/null 2>&1
+cd
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/VpnChanger/plugin.py "https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/MonsterVPN/plugin.py" &> /dev/null 2>&1
+
+# Install OpenVPN
+echo "Installing OpenVPN"
+echo $LINE
+opkg update &> /dev/null 2>&1
+opkg --force-reinstall --force-overwrite install openvpn &> /dev/null 2>&1
+
+# Download VPN Configs
+echo "Downloading OpenVPN Configs"
+echo $LINE
+wget -O /tmp/auth.txt "https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/NordVPN/password.conf" &> /dev/null 2>&1
+wget -O /hdd/MonsterVPN/MonsterVPN.zip "http://www.monstervpn.tech/ovpn_configuration.zip" &> /dev/null 2>&1
+
+# Configure VPN
+echo "Configuring OpenVPN"
+cd /hdd/MonsterVPN
+unzip -o MonsterVPN.zip &> /dev/null 2>&1
+rm -v /hdd/MonsterVPN.zip &> /dev/null 2>&1
+rm -rv /hdd/MonsterVPN/__MACOSX >/dev/null 2>&1
+
+
+# rename .ovpn to .conf
+for x in *.ovpn; do mv "$x" "${x%.ovpn}.conf"; done
+
+# Edit all conf files to have auth-user-pass/auth-user-pass auth.txt
+find . -name "*.conf" -exec sed -i "s/auth-user-pass/auth-user-pass auth.txt/g" '{}' \;
+
+# Move all files into sub folders
+for file in *; do
+  if [[ -f "$file" ]]; then
+    mkdir "${file%.*}"
+    mv "$file" "${file%.*}"
+  fi
+done
+
+cd
+echo $LINE
+#Add username and password to auth.txt
+sed -i -e "s/USERNAME/$USERNAME/g" /tmp/auth.txt;sed -i -e "s/PASSWORD/$PASSWORD/g" /tmp/auth.txt && chmod 777 /tmp/auth.txt &> /dev/null 2>&1
+
+# Copy auth.txt to MonsterVPN sub folders
+find /hdd/MonsterVPN -type d -exec cp /tmp/auth.txt {} \;
+# Delete uneeded files 
+rm -f /hdd/MonsterVPN/auth.txt &> /dev/null 2>&1
+rm -f /tmp/auth.txt &> /dev/null 2>&1
+rm -f /home/root/MonsterVPN.sh &> /dev/null 2>&1
+echo "OpenVPN Configs Downloaded Please Start OpenVPN"
+exit
+fi
