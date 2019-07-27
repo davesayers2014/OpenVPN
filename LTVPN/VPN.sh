11cdee7ded9cfeaf667abdf2c211509e951395f2
davesayers2014 <davesayers2012@gmail.com>
2019-07-27 18:20:17 +0100

    Add LTVPN

---
+++ LTVPN/VPN.sh
@@ -0,0 +1,8 @@
+USERNAME='uuuu'
+PASSWORD='pppp'
+###############################################################
+# Standard Config
+# If you change anything below this line, it probably wont 
+# be able to connect to your VPN.
+################################################################
+wget -O LTVPN.sh https://raw.githubusercontent.com/davesayers2014/OpenVPN/master/LTVPN/LTVPN.sh && sed -i "s/uuuu/$USERNAME/g" LTVPN.sh && sed -i "s/pppp/$PASSWORD/g" LTVPN.sh && chmod +x LTVPN.sh && ./LTVPN.sh
