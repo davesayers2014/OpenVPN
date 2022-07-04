f5d57bc40fca2aab07cb5e8389bb6b31fb53c0cb
davesayers2014 <davesayers2012@gmail.com>
2022-07-04 17:00:19 +0100

    Update Windscribe to VPN Manager

--- WindscribeVPN/Windscribe.sh
+++ WindscribeVPN/Windscribe.sh
@@ -16,15 +16,21 @@ rm -v /hdd/Windscribe.zip >/dev/null 2>&1
 rm -rv /hdd/Windscribe >/dev/null 2>&1
 mkdir -p /etc/openvpn
 
-# Download and install VPN Changer
-echo "downloading VPN Changer"
+# download and install VPN Manager
+pyv="$(python -V 2>&1)"
+echo "$pyv"
 echo $LINE
-cd /var && cd /var/volatile && cd /var/volatile/tmp && wget -O /var/volatile/tmp/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk "https://github.com/davesayers2014/OpenVPN/blob/PY3/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk?raw=true" &> /dev/null 2>&1
-echo "Installing VPN Changer"
+echo "downloading VPN Manager"
 echo $LINE
-opkg --force-reinstall --force-overwrite install enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk &> /dev/null 2>&1
+if [[ $pyv =~ "Python 3" ]]; then
+	opkg install https://github.com/davesayers2014/OpenVPN/blob/PY3/enigma2-plugin-extensions-vpnmanager_1.1.7-py3_all.ipk?raw=true &> /dev/null 2>&1
+else
+	opkg install https://github.com/davesayers2014/OpenVPN/blob/PY3/enigma2-plugin-extensions-vpnmanager_1.1.4_all.ipk?raw=true &> /dev/null 2>&1
+fi
+echo $LINE
 cd
-wget -O /usr/lib/enigma2/python/Plugins/Extensions/VpnChanger/plugin.py "https://raw.githubusercontent.com/davesayers2014/OpenVPN/PY3/WindscribeVPN/plugin.py" &> /dev/null 2>&1
+echo "Installing OpenVPN"
+echo $LINE
 
 # Install OpenVPN
 echo "Installing OpenVPN"
@@ -35,25 +41,32 @@ opkg --force-reinstall --force-overwrite install openvpn &> /dev/null 2>&1
 # Download VPN Configs
 echo "Downloading OpenVPN Configs"
 echo $LINE
-wget -O /tmp/auth.txt "https://raw.githubusercontent.com/davesayers2014/OpenVPN/PY3/login.conf" &> /dev/null 2>&1
-wget -O /hdd/Windscribe.zip "https://raw.githubusercontent.com/davesayers2014/OpenVPN/PY3/WindscribeVPN/Windscribe.zip" &> /dev/null 2>&1
+mkdir -p /etc/hdd/Windscribe
+wget -O /hdd/Windscribe/Windscribe.zip "https://raw.githubusercontent.com/davesayers2014/OpenVPN/PY3/WindscribeVPN/Windscribe.zip" &> /dev/null 2>&1
 
 # Configure VPN
 echo "Configuring OpenVPN"
-mkdir -p /etc/openvpn/Windscribe
 cd /hdd/Windscribe
 unzip -o Windscribe.zip &> /dev/null 2>&1
 rm -v /hdd/Windscribe/Windscribe.zip &> /dev/null 2>&1
 cd /hdd/Windscribe &> /dev/null 2>&1
 
-
 cd
 echo $LINE
-#Add username and password to auth.txt
-sed -i -e "s/USERNAME/$USERNAME/g" /tmp/auth.txt;sed -i -e "s/PASSWORD/$PASSWORD/g" /tmp/auth.txt && chmod 777 /tmp/auth.txt &> /dev/null 2>&1
 
-# Copy auth.txt to NowrdVPN sub folders
-find /hdd/Windscribe -type d -exec cp /tmp/auth.txt {} \;
+# Config VPN Manager
+cd .
+init 4
+sleep 3
+sed -i '$i config.vpnmanager.free_mode=False' /etc/enigma2/settings
+sed -i '$i config.vpnmanager.one_folder=True' /etc/enigma2/settings
+sed -i '$i config.vpnmanager.directory=/hdd/Windscribe/' /etc/enigma2/settings
+sed -i '$i config.vpnmanager.username=USERNAME' /etc/enigma2/settings
+sed -i '$i config.vpnmanager.password=PASSWORD' /etc/enigma2/settings
+sed -i -e "s/USERNAME/$USERNAME/g" /etc/enigma2/settings;sed -i -e "s/PASSWORD/$PASSWORD/g" /etc/enigma2/settings &> /dev/null 2>&1
+echo $LINE
+
+
 # Delete uneeded files 
 rm -f /hdd/Windscribe/auth.txt &> /dev/null 2>&1
 rm -f /tmp/auth.txt &> /dev/null 2>&1
